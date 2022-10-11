f2409a6a3c01921f9245355b69c38bbdd80fcd5e
ShafayetZim <shafayet.zim@northsouth.edu>
2022-10-11 10:50:26 +0600

    model register file

---
+++ dataset/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(Continent)
+admin.site.register(Country)
+admin.site.register(Weight)
+admin.site.register(Zone)
+admin.site.register(DollarRate)
+admin.site.register(Courier)
+admin.site.register(ServiceProvider)
+admin.site.register(Pricing)
+admin.site.register(CommissionSetting)
+admin.site.register(ZoneSetting)
